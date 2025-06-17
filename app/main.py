52883fdb91825f611d3bc634caa9f5c44ed3237d
sasha <sasha.friis@icloud.com>
2025-06-17 23:50:21 +0200

    Added middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from .routers import products
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    #Url allowed to access the API
+    allow_origins=["http://localhost:3000"],  #React URL
+    #When true allows cookies and authtication headers
+    allow_credentials=True,
+    #Allows all methods (GET, POST, PUT, DELETE, etc.)
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    #Allows all headers (Content-Type, Authorization, etc.)
+    allow_headers=[
+        "Content-Type", #Needed for JSON requests
+        "Authorization", #Required for token-based auth
+        "Access-Control-Allow-Headers", #CORS speification
+        "Origin", #Browser sends origin header
+        "Accept" #Specifies acceptable response format
+    ]
+)
 
 # Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
